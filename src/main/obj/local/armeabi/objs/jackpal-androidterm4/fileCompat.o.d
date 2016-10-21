d66f3a5e1bb70ebe4d95dd53f3feb2fcefeb9801
River <river@quseit.com>
2016-10-21 12:13:59 +0800

    First initial

---
+++ src/main/obj/local/armeabi/objs/jackpal-androidterm4/fileCompat.o.d
@@ -0,0 +1,131 @@
+/Users/yhc/MobileDev/AS/qpython/qconsole/src/main/obj/local/armeabi/objs/jackpal-androidterm4/fileCompat.o: \
+ /Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/fileCompat.cpp \
+ /Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/common.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/fileCompat.h
+
+/Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/common.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/fileCompat.h:
---
+++ src/main/obj/local/armeabi/objs/jackpal-androidterm4/termExec.o.d
@@ -0,0 +1,224 @@
+/Users/yhc/MobileDev/AS/qpython/qconsole/src/main/obj/local/armeabi/objs/jackpal-androidterm4/termExec.o: \
+ /Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/termExec.cpp \
+ /Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/common.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ioctl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/ioctl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ioctl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ioctls.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/termbits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/wait.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/resource.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/resource.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/resource.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/wait.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/unistd.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syscall.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/termios.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/termios.h \
+ /Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/termios.h \
+ /Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/termExec.h
+
+/Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/common.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ioctl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/ioctl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ioctl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ioctls.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/termbits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/wait.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/resource.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/resource.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/resource.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/wait.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/unistd.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syscall.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/termios.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/termios.h:
+
+/Users/yhc/MobileDev/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/termios.h:
+
+/Users/yhc/MobileDev/AS/qpython/qconsole/src/main/jni/termExec.h:
